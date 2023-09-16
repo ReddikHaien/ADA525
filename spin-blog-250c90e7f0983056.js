2f725375e3f09b2d800b17a3ad44bd6ba2bc388c
ReddikHaien <33747157+ReddikHaien@users.noreply.github.com>
2023-09-16 12:19:38 +0000

    Update gh-pages to output generated at 2cf4a40

--- spin-blog-250c90e7f0983056.js
+++ spin-blog-250c90e7f0983056.js
@@ -931,7 +931,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('spin-blog-895ac29be92150b5_bg.wasm', import.meta.url);
+        input = new URL('spin-blog-250c90e7f0983056_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
