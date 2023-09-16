d4202053c28ff2db71cba345eb7d41b0e0cc6988
ReddikHaien <33747157+ReddikHaien@users.noreply.github.com>
2023-09-16 13:04:18 +0000

    Update gh-pages to output generated at 7641419

--- spin-blog-c5c4f79f0fc715ea.js
+++ spin-blog-c5c4f79f0fc715ea.js
@@ -931,7 +931,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('spin-blog-250c90e7f0983056_bg.wasm', import.meta.url);
+        input = new URL('spin-blog-c5c4f79f0fc715ea_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
